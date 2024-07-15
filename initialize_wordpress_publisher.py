68aeaca823890fa88e3d4f1b5734b28e61f11c54
Paul Cutcliffe <Paul.Cutcliffe@Gmail.com>
2024-07-15 03:17:46 +0100

    Reformatted prompts (removed line breaks) so they look okay on blog.

--- initialize_wordpress_publisher.py
+++
@@ -1,8 +0,0 @@
-import WordPressPublisher
-
-# Initialize WordPress publisher
-wp_publisher = WordPressPublisher(
-    "https://cctvfun.cutterz68.com",
-    "CCTVFun",
-    "NS6M Jpza LXvt 4d92 We4L 0psD" # Probably need the spaces to be removed?
-)
